383e7be042ad434454b36d19f28b6fbd00d09705
Lázaro José Pedrosa dos Reis <lazaro.pedrosa@outlook.com>
2023-04-27 22:58:09 -0300

    Finalizado

--- Gauss Jacobi.py
+++ Gauss Jacobi.py
@@ -1,3 +1,43 @@
 #Implementação Método Iterativo Gauss Jacobi
 #Autor: lazaro jose Pedrosa dos Reis
 #2023
+
+def gaussJacobi(A,b,x,iteracoes,e):
+    for i in range(iteracoes):
+        for j in range(len(A)):
+            soma = 0
+            for k in range(len(A)):
+                if k != j:
+                    soma += A[j][k] * x[k]
+            x[j] = (b[j] - soma) / A[j][j]
+        if e > abs(x[0] - x[1]) and e > abs(x[1] - x[2]):
+            return x
+    return x
+        
+def main():
+    A = [[10, 2, 1], 
+         [1, 5, 1], 
+         [2, 3, 10]]
+    b = [7, -8, 6]
+    x = [0.7, -1.6, 0.6]
+    e = 0.05
+    iteracoes = 10
+    print("____ALGORITMO DE GAUSS JACOBI_____\n")
+    print("Matriz A: ")
+    for k in A: print('\t'.join(map(str, k)))
+    print("\nVetor b: ")
+    print(b)
+    print("\nVetor x inicial: ")
+    print(x)
+    print("\nValor de tolerância: ")
+    print(e)
+    print("\nNúmero de iterações: ")
+    print(iteracoes)
+    print("\nResultado: ")
+    res = gaussJacobi(A,b,x,iteracoes,e)
+    print(res)
+    print("\nResultado com arredondamento: ")
+    print(round(res[0],2),round(res[1],2),round(res[2],2))
+    print("\n____________________________________")
+
+main()
